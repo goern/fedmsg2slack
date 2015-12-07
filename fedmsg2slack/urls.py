9d850f50b27de03b98b36d217071fd9292b280e9
Christoph Görn <goern@b4mad.net>
2015-12-07 20:35:20 +0100

    .

---
+++ fedmsg2slack/urls.py
@@ -0,0 +1,13 @@
+from django.conf.urls import patterns, include, url
+
+from django.contrib import admin
+admin.autodiscover()
+
+import hello.views
+
+urlpatterns = patterns('',
+    url(r'^$', hello.views.index, name='index'),
+    url(r'^db', hello.views.db, name='db'),
+    url(r'^admin/', include(admin.site.urls)),
+
+)
